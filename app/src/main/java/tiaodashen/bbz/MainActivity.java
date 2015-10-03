c6f9d4686fc31a49275939616d5bfc3bf5889184
Side <lisided@gmail.com>
2015-10-03 09:06:15 -0700

    1st commit

--- app/src/main/java/tiaodashen/bbz/MainActivity.java
+++ app/src/main/java/tiaodashen/bbz/MainActivity.java
@@ -1,7 +1,8 @@
 package tiaodashen.bbz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.v("main","Test");
         return true;
     }
 
