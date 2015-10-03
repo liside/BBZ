810ddfcf16972c12c5d7d56e20283fe24e6a2346
Side <lisided@gmail.com>
2015-10-03 13:27:10 -0700

    add wifi

--- app/src/main/java/tiaodashen/bbz/MainActivity.java
+++ app/src/main/java/tiaodashen/bbz/MainActivity.java
@@ -1,10 +1,13 @@
 package tiaodashen.bbz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.start);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent game = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(game);
+            }
+        });
     }
 
     @Override
