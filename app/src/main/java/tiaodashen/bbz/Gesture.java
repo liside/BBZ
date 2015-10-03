72c300daf1cb1dd0ae0f59b6e3a8de776c6c8d90
XavierWang <reedyangel@gmail.com>
2015-10-03 12:25:06 -0700

    add gesture

--- app/src/main/java/tiaodashen/bbz/Gesture.java
+++ app/src/main/java/tiaodashen/bbz/Gesture.java
@@ -3,26 +3,44 @@ package tiaodashen.bbz;
 /**
  * Created by XavierWang on 15/10/3.
  */
+import java.lang.Number;
+
 public abstract class Gesture {
 
         private String name;
+        private int charge;
 
         public Gesture() {
             setName("Gesture");
+            this.charge = 0;
         }
 
-        public Gesture( String name ) {
+
+        public Gesture( String name, int charge ) {
             setName(name);
+            setCharge(charge);
         }
 
-        public abstract void compareTo( Gesture gesture );
+        public abstract int compareTo( Gesture gesture );
+
+        public int chargeChange(){
+            return 0;
+        }
 
         private void setName( String name ) {
             this.name = name;
         }
 
+        private void setCharge( int charge ) {
+            this.charge = charge;
+        }
+
         public String getName() {
             return this.name;
+        }
+
+        public int getCharge() {
+            return this.charge;
         }
 
 }
